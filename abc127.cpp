b5b4d4add57019b685800c14ca3067cc7a47ac12
skymer156 <46322826+skymer156@users.noreply.github.com>
2021-05-23 17:18:05 +0900

    update practice code

---
+++ abc127.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    long N, M;
+    cin >> N >> M;
+    vector<long> L;
+    vector<long> R;
+
+    for (int i = 0; i < M; i++)
+    {
+        long l, r;
+        cin >> l >> r;
+        L.push_back(l);
+        R.push_back(r);
+    }
+
+    sort(L.begin(), L.end());
+    sort(R.begin(), R.end());
+
+    if (max - min < 0)
+    {
+        cout << 0 << endl;
+    }
+    else
+    {
+        cout << max - min + 1 << endl;
+    }
+
+    return 0;
+}
