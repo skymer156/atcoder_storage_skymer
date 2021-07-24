f543421606649bf5061731fc5190058c570f2fc5
skymer156 <46322826+skymer156@users.noreply.github.com>
2021-07-25 00:06:17 +0900

    漸化式、dpを使用。youtube解説動画を見て復習。 番兵法をさらっと組み込めるのはすごいので真似していきたい。dpは簡単な題を想定して図を書くのが良い。

--- abc211c.cpp
+++ abc211c.cpp
@@ -1,11 +1,31 @@
+// re-learning
+
 #include <iostream>
 #include <vector>
+
+#define rep(i,n) for (int i = 0; i < (n); ++i)
 
 using namespace std;
 
 int main(){
     string s;
     cin >> s;
+
+    int N = s.size();
+
+    vector<vector<int>> dp(N+1,vector<int>(9));
+    rep(i,N+1) dp[i][0] = 1;
+    const int mod = 1000000007;
+    string t = "chokudai";
+    rep(i,N)rep(j,8) {
+        if(s[i] != t[j]){
+            dp[i+1][j+1] = dp[i][j+1];
+        }else{
+            dp[i+1][j+1] =  ( dp[i][j+1] + dp[i][j] ) % mod;
+        }
+    }
+
+    cout << dp[N][8] << endl;
 
     return 0;
 }
