757b0e697e5474afc261e561c57f17b91cf5af42
skymer156 <46322826+skymer156@users.noreply.github.com>
2022-06-09 20:32:00 +0900

    add abc249

--- abc249/a.cpp
+++ abc249/a.cpp
@@ -1,7 +1,40 @@
-#include <stdio.h>
+#include <bits/stdc++.h>
+using namespace std;
+
+int meter( int A, int B, int C, int X ){
+	int meter;
+  	int a, b;
+  a = X / (A + C);
+  if (a == 0){
+  	if( X < A ){
+    	meter = B * X;
+    }else{
+    	meter = A * B;
+    }
+  }else{
+    b = X % (A + C);
+	if( b < A ){
+    	meter = a * B * A + B * b;
+    }else{
+    	meter = a * B * A + A * B;
+    }
+  }
+  return meter;
+}
 
 int main() {
-	printf("Hello world!!");
-
-	return 0;
+    int A, B, C, D, E, F, X;
+    cin >> A >> B >> C >> D >> E >>F >> X;
+    int meter_t, meter_a;
+  meter_t = meter(A, B, C, X);
+  meter_a = meter(D, E, F, X);
+  if (meter_t > meter_a){
+    cout << "Takahashi" << endl;
+  }else if(meter_a > meter_t){
+    cout << "Aoki" << endl;
+  }else{
+  	cout << "Draw" << endl;
+  }
+  
+  return 0;
 }
